67f0887af912a9922d81ce603d4d18348f546c1b
Aleksei Shevelev <shevelsm@gmail.com>
2023-03-26 18:44:41 +0300

    Implement get_the_last_log_file function

--- hw1-advanced_basics/log_analyzer/log_analyzer.py
+++ hw1-advanced_basics/log_analyzer/log_analyzer.py
@@ -1,11 +1,12 @@
 import argparse
 import configparser
+from datetime import datetime
 import logging
+import os
 import re
 import sys
-from collections import namedtuple
-from os.path import exists
 import traceback
+from collections import namedtuple
 from typing import Optional
 
 
@@ -19,11 +20,6 @@ from typing import Optional
 #                     '$request_time';
 
 config = {"REPORT_SIZE": 1000, "REPORT_DIR": "./reports", "LOG_DIR": "./log"}
-
-
-LOG_DATE_REGEXP = re.compile(
-    r"(?P<full_date>(?P<year>\d{4})(?P<month>\d{2})(?P<day>\d{2}))"
-)
 
 
 def parse_arguments() -> argparse.Namespace:
@@ -52,10 +48,10 @@ def get_config_values(
         config.read(config_path, encoding="utf-8")
 
     try:
-        report_size = config.get("LOG_ANALYZER", "REPORT_SIZE")
-        report_dir = config.get("LOG_ANALYZER", "REPORT_DIR")
-        log_dir = config.get("LOG_ANALYZER", "LOG_DIR")
-    except configparser.NoOptionError:
+        report_size = config.get("DEFAULT", "REPORT_SIZE")
+        report_dir = config.get("DEFAULT", "REPORT_DIR")
+        log_dir = config.get("DEFAULT", "LOG_DIR")
+    except (configparser.NoOptionError, configparser.NoSectionError):
         return None
 
     return ReportConfig(report_size, report_dir, log_dir)
@@ -71,26 +67,67 @@ def init_logging_config(filename: Optional[str] = None, level: str = "INFO") -> 
             level=getattr(logging, level),
         )
     except TypeError:
-        logging.error('Error initializing the logging system')
+        logging.error("Error initializing the logging system")
         traceback.print_stack()
         return False
 
     return True
 
 
+def get_the_last_log_file(config: type) -> str:
+    dt_format = "%Y%m%d"
+    dt_regex = re.compile(
+        r"^nginx-access-ui\.log-(?P<full_date>(?P<year>\d{4})(?P<month>\d{2})(?P<day>\d{2}))"
+    )
+    log_dir = config.log_dir
+    if not log_dir or not os.path.exists(log_dir) or not os.path.isdir(log_dir):
+        logging.error("Log directory was not found. LOG_DIR: {}".format(log_dir))
+        return None
+
+    last_filename = None
+    last_date = datetime.strptime("19700101", dt_format)
+    LastLog = namedtuple("LastLog", ["filename", "date"])
+
+    for filename in os.listdir(log_dir):
+        path = os.path.join(log_dir, filename)
+        m = dt_regex.match(filename)
+
+        try:
+            dt = datetime.strptime(m.group("full_date"), dt_format)
+        except ValueError:
+            logging.error(
+                "Bad log_date ({}) for log_file {}".format(
+                    filename, m.group("full_date")
+                )
+            )
+            continue
+
+        if dt and dt > last_date:
+            last_filename = filename
+            last_date = dt
+
+    return LastLog(last_filename, last_date)
+
+
 def main() -> None:
     args = parse_arguments()
-    if not exists(args.config_path):
+    if args.config_path and not os.path.exists(args.config_path):
         sys.exit("No such config file!")
 
     report_config = get_config_values(config, args.config_path)
     if not report_config:
         sys.exit("There is no valid config!")
 
-    if not init_logging_config(filename='1'):
+    if not init_logging_config():
         sys.exit("Check init_logging_config() usage!")
 
-    logging.info('Log analyzer script has finished the work!')
+    LastLog = get_the_last_log_file(report_config)
+    if not LastLog:
+        sys.exit()
+
+    logging.debug("The last log file is {}".format(LastLog.filename))
+
+    logging.info("Log analyzer script has finished the work!")
 
 
 if __name__ == "__main__":
