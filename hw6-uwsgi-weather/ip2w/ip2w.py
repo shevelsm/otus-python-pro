c086ad250b5a8a9a22077c1e867caeb1f943c238
Aleksei Shevelev <shevelsm@gmail.com>
2023-05-04 10:07:43 +0300

    Init logging for HW3 UWSGI daemon

--- hw6-uwsgi-weather/ip2w/ip2w.py
+++ hw6-uwsgi-weather/ip2w/ip2w.py
@@ -1,4 +1,25 @@
 import socket
+import logging
+import traceback
+from typing import Optional
+
+
+def init_logging_config(filename: Optional[str] = None, level: str = "INFO") -> None:
+    try:
+        logging.basicConfig(
+            filename=filename,
+            filemode="a",
+            format="[%(asctime)s] %(levelname).1s %(message)s",
+            datefmt="%Y.%m.%d %H:%M:%S",
+            level=getattr(logging, level),
+        )
+    except TypeError:
+        logging.error("Error initializing the logging system")
+        traceback.print_stack()
+        return False
+
+    return True
+
 
 def check_valid_ip(ip_addres: str) -> bool:
     try:
@@ -9,7 +30,9 @@ def check_valid_ip(ip_addres: str) -> bool:
 
 
 def application(env, start_response):
+    init_logging_config("ip2w.log")
     ip_address = env["REQUEST_URI"].replace("/ip2w/", "")
+    logging.debug(env)
     response = check_valid_ip(ip_address)
     start_response("200 OK", [("Content-Type", "text/html")])
     return [bytes(response, encoding="utf-8")]
