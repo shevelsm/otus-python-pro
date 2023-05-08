452c242c647c3340c6efb2e7d84d030405a5c670
Aleksei Shevelev <shevelsm@gmail.com>
2023-05-08 17:50:41 +0300

    Add encode("UTF-8") for hexdigest

--- hw3-oop-scoring/test.py
+++ hw3-oop-scoring/test.py
@@ -28,10 +28,11 @@ class TestSuite(unittest.TestCase):
 
     def set_valid_auth(self, request):
         if request.get("login") == api.ADMIN_LOGIN:
-            request["token"] = hashlib.sha512(datetime.datetime.now().strftime("%Y%m%d%H") + api.ADMIN_SALT).hexdigest()
+            check_token = datetime.datetime.now().strftime("%Y%m%d%H") + api.ADMIN_SALT
+            request["token"] = hashlib.sha512(check_token.encode("UTF-8")).hexdigest()
         else:
             msg = request.get("account", "") + request.get("login", "") + api.SALT
-            request["token"] = hashlib.sha512(msg).hexdigest()
+            request["token"] = hashlib.sha512(msg.encode("UTF-8")).hexdigest()
 
     def test_empty_request(self):
         _, code = self.get_response({})
