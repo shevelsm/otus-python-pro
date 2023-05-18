d6949d20aad18ed2fe763f08538e7387467a5ed1
Aleksei Shevelev <shevelsm@gmail.com>
2023-05-18 20:45:38 +0300

    Add retries to the Redis store

--- hw3.1-scoring-tests/store.py
+++ hw3.1-scoring-tests/store.py
@@ -1,42 +1,48 @@
-import redis
+from redis.backoff import ExponentialBackoff
+from redis.retry import Retry
+from redis.client import Redis
+from redis.exceptions import BusyLoadingError, ConnectionError, TimeoutError
 
 
 class RedisAsStorage:
     def __init__(
-            self,
-            host: str='localhost',
-            port: int=6379,
-            db: int=0,
-            timeout: int=3,
-            connect_now: bool=True
-        ) -> None:
-            self.host = host
-            self.port = port
-            self.db = db
-            self.timeout = timeout
+        self,
+        host: str = "localhost",
+        port: int = 6379,
+        db: int = 0,
+        timeout: int = 5,
+        retries: int = 5,
+        connect_now: bool = True,
+    ) -> None:
+        self.host = host
+        self.port = port
+        self.db = db
+        self.timeout = timeout
+        self.retries = retries
 
-            if connect_now is True:
-                  self.connect()
+        if connect_now is True:
+            self.connect()
 
     def connect(self):
-        self.con = redis.Redis(
+        self.con = Redis(
             host=self.host,
             port=self.port,
             db=self.db,
+            retry=Retry(ExponentialBackoff(), self.retries),
+            retry_on_error=[BusyLoadingError, ConnectionError, TimeoutError],
             socket_connect_timeout=self.timeout,
-            socket_timeout=self.timeout
+            socket_timeout=self.timeout,
         )
-    
+
     def get(self, key):
         try:
             value = self.con.get(key)
             return value.decode() if value else value
         except:
-               raise ConnectionError
-    
+            raise ConnectionError
+
     def set(self, key, value, ttl):
         try:
             return self.con.set(key, value, ex=ttl)
         except:
-             raise ConnectionError
-        
+            raise ConnectionError
