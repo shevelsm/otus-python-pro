aa3d5b5e153057468bda113c093d243cbbe094c6
Aleksei Shevelev <shevelsm@gmail.com>
2023-06-04 13:44:37 +0300

    Implement get_city_by_ip part fo HW 6

--- hw6-uwsgi-weather/ip2w/ip2w.py
+++ hw6-uwsgi-weather/ip2w/ip2w.py
@@ -1,7 +1,20 @@
-import socket
 import logging
+import socket
 import traceback
-from typing import Optional
+from configparser import ConfigParser
+from typing import Any, Optional, Tuple, Union
+
+import requests
+
+CONFIG_PATH = "ip2w.ini"
+IPINFO_URL = "https://ipinfo.io/{}"
+OPENWEATHER_URL = (
+    "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&lang=ru&APPID={}"
+)
+
+HTTP_200_OK = 200
+HTTP_400_BAD_REQUEST = 400
+HTTP_500_INTERNAL_ERROR = 500
 
 
 def init_logging_config(filename: Optional[str] = None, level: str = "INFO") -> None:
@@ -21,21 +34,56 @@ def init_logging_config(filename: Optional[str] = None, level: str = "INFO") -> 
     return True
 
 
-def check_valid_ip(ip_addres: str) -> bool:
+def get_config_values(config_path: str = None) -> ConfigParser:
+    config = ConfigParser()
+    config.read(config_path, encoding="utf-8")
+
+    return config
+
+
+def check_valid_ip(ip: str) -> bool:
     try:
-        socket.inet_aton(ip_addres)
+        socket.inet_aton(ip)
         return True
     except socket.error:
         return False
 
 
+def perform_request(url: str, config: ConfigParser) -> Tuple[int, Union[dict, str]]:
+    retries = int(config["ip2w"]["retries"])
+    timeout = int(config["ip2w"]["timeout"])
+    for _ in range(retries):
+        response = requests.get(url, timeout=timeout)
+        response.raise_for_status()
+        return HTTP_200_OK, response.json()
+    return HTTP_500_INTERNAL_ERROR, "Maximum retries exceeded"
+
+
+def get_city_by_ip(ip: str, config: ConfigParser) -> Union[int, str]:
+    code, response = perform_request(IPINFO_URL.format(ip), config)
+    if code != HTTP_200_OK:
+        return code, response
+
+    city = response.get("city")
+    country = response.get("country")
+    if not (city and country):
+        return HTTP_500_INTERNAL_ERROR, "No country and city for the {}".format(ip)
+
+    return HTTP_200_OK, "{},{}".format(city, country)
+
+
 def application(env, start_response):
-    init_logging_config("ip2w.log")
+    config = get_config_values(CONFIG_PATH)
+    init_logging_config(config["ip2w"]["log_path"], config["ip2w"]["log_level"])
     ip_address = env["REQUEST_URI"].replace("/ip2w/", "")
-    logging.debug(env)
-    if check_valid_ip(ip_address):
-        response = ip_address
+    logging.debug("IP address from the request - {}".format(ip_address))
+    if not check_valid_ip(ip_address):
+        code, response = (
+            HTTP_400_BAD_REQUEST,
+            "Incorrect IP address - {}. Please try another one...".format(ip_address),
+        )
     else:
-        response = "Bad IP"
-    start_response("200 OK", [("Content-Type", "text/html")])
+        code, response = get_city_by_ip(ip_address, config)
+
+    start_response(str(code), [("Content-Type", "text/html")])
     return [bytes(response + "\n", encoding="utf-8")]
